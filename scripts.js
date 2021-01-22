367c3a740da4c2711bed684b05b46c03080c6242
Devlin <devlincodes@gmail.com>
2021-01-21 22:30:16 -0800

    Fixed navbar alignment, added background to projects section

--- scripts.js
+++ scripts.js
@@ -10,25 +10,37 @@ let topOfNav;
 
 // FUNCTIONS
 
+// NAV FUNCTIONS:
+
 function handleEnter() {
-    this.classList.add('.trigger-enter')
-    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    this.classList.add('.trigger-enter');
+    setTimeout(() => {
+        if (this.classList.contains('trigger-enter')) {
+            this.classList.add('trigger-enter-active');
+        };
+    }, 150);
     dropdownBackground.classList.add('open');
-}
+    
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left,
+    };
+
+    dropdownBackground.style.setProperty('width', `${coords.width}px`);
+    dropdownBackground.style.setProperty('height', `${coords.height}px`);
+    dropdownBackground.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+};
 
 function handleLeave() {
-    this.classList.remove('trigger-enter');
-    setTimeout(() => this.classList.remove('trigger-enter-active'), 150);
+    this.classList.remove('.trigger-enter', '.trigger-enter-active');
     dropdownBackground.classList.remove('open');
-
-}
-
-
-
-
-
-
-
+};
 
 function findNav() {
     topOfNav = nav.offsetTop;
