7909037dd3db5b7ddeae49a03683ce667391645e
Devlin <devlincodes@gmail.com>
2021-01-23 08:28:56 -0800

    Fixed navigation bar functionality with dropdown menus, removed redundant code

--- scripts.js
+++ scripts.js
@@ -4,67 +4,74 @@ const intro = document.querySelector('#intro');
 const nav = document.querySelector('#nav');
 const triggers = document.querySelectorAll('.nav-dropdown');
 const dropdownBackground = document.querySelector('.dropdown-background');
+const navDropdown = document.querySelectorAll('.nav-dropdown');
 const projectsButton = document.querySelector('#nav-projects');
 
 let topOfNav;
 
 // FUNCTIONS
 
+function prevent(e) {
+    e.preventDefault();
+}
+
 // NAV FUNCTIONS:
 
-function handleEnter() {
-    this.classList.add('.trigger-enter');
-    console.log('enter')
+function findNav() {
+    topOfNav = nav.offsetTop;
+};
+
+function fixNav() {
+    if (window.scrollY >= topOfNav) {
+        //document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+        projectsButton.firstChild.nextElementSibling.textContent = 'About Me';
+    } else {
+        nav.style.bottom = window.scrollY + 'px';
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+        projectsButton.firstChild.nextElementSibling.textContent = 'Projects';
+    };
+};
+
+function handleEnter(e) {
+    e.preventDefault();
+    console.log(this)
+    this.classList.add('trigger-enter');
     setTimeout(() => {
-        if (this.classList.contains('.trigger-enter')) {
-            this.classList.add('.trigger-enter-active');
+        if (this.classList.contains('trigger-enter')) {
+            this.classList.add('trigger-enter-active');
         };
     }, 150);
     dropdownBackground.classList.add('open');
     
     const dropdown = this.querySelector('.dropdown');
+    console.log(dropdown)
     const dropdownCoords = dropdown.getBoundingClientRect();
     const navCoords = nav.getBoundingClientRect();
-
+    console.log(dropdownCoords)
+    
     const coords = {
         height: dropdownCoords.height,
         width: dropdownCoords.width,
         top: dropdownCoords.top - navCoords.top,
         left: dropdownCoords.left - navCoords.left,
     };
-
+    console.log(dropdownBackground)
+    console.log(coords)
     dropdownBackground.style.setProperty('width', `${coords.width}px`);
     dropdownBackground.style.setProperty('height', `${coords.height}px`);
     dropdownBackground.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
 };
 
 function handleLeave() {
-    this.classList.remove('.trigger-enter', '.trigger-enter-active');
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
     dropdownBackground.classList.remove('open');
 };
 
-function findNav() {
-    topOfNav = nav.offsetTop;
-    console.log(topOfNav);
-};
-
-function fixNav() {
-    console.log(topOfNav, window.scrollY)
-    if (window.scrollY >= topOfNav) {
-        //document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed-nav');
-        projectsButton.textContent = 'About Me';
-    } else {
-        nav.style.bottom = window.scrollY + 'px';
-        document.body.style.paddingTop = 0;
-        document.body.classList.remove('fixed-nav');
-        projectsButton.textContent = 'Projects';
-    };
-};
-
-function scroll() {
+function scroll(e) {
+    e.preventDefault();
     if (window.scrollY < topOfNav) {
-        console.log(topOfNav)
         document.documentElement.scrollTop = topOfNav;
     } else {
         document.documentElement.scrollTop = 0;
@@ -77,4 +84,5 @@ document.addEventListener('DOMContentLoaded', findNav);
 window.addEventListener('scroll', fixNav);
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
 triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
-projectsButton.addEventListener('click', scroll)
+projectsButton.addEventListener('click', scroll);
+navDropdown.forEach(navButton => navButton.addEventListener('click', prevent));
